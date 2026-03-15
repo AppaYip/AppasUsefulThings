1b58bf7c7ca7cadfed46df5026f2ab823d944765
Appa <1pewdiepieisbetter1@gmail.com>
2026-03-14 20:39:36 -0400

    Fixed build.gradle, added new class: ItemBuilder

---
+++ src/main/java/org/appa/appasUsefulThings/Items/ItemBuilder.java
@@ -0,0 +1,148 @@
+package org.appa.appasUsefulThings.Items;
+
+import net.kyori.adventure.text.TextComponent;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class ItemBuilder {
+    private final Material material;
+
+    // Main
+    private TextComponent name;
+    private int amount = 1;
+    private ArrayList<TextComponent> lore = new ArrayList<>();
+
+    // Appearance
+    private Integer customModelData = null;
+    private ItemFlag[] itemFlags;
+
+    // Misc
+    private boolean unbreakable = false;
+    private int damage = 0;
+    private HashMap<Enchantment, Integer> enchants = new HashMap<>();
+
+
+
+    /*
+     * Constructors
+     */
+    public ItemBuilder(Material material, TextComponent name, int damage) {
+        this.material = material;
+        this.name = name;
+        this.damage = damage;
+    }
+
+    public ItemBuilder(Material material, TextComponent name) {
+        this.material = material;
+        this.name = name;
+    }
+
+    public ItemBuilder(Material material, int damage) {
+        this.material = material;
+        this.damage = damage;
+    }
+
+
+    public ItemBuilder(Material material) {
+        this.material = material;
+    }
+
+
+    /*
+     * Main Things
+     * Name, Amount, Lore
+     */
+    public ItemBuilder setDisplayName(TextComponent name) {
+        this.name = name;
+        return this;
+    }
+
+    public ItemBuilder setAmount(int amount) {
+        this.amount = amount;
+        return this;
+    }
+
+
+    public ItemBuilder addLore(TextComponent... line) {
+        this.lore.addAll(Arrays.asList(line));
+        return this;
+    }
+
+
+    /*
+     * Appearance
+     * Custom Model Data, Item Flags
+     */
+    public ItemBuilder setCustomModelData(int customModelData) {
+        this.customModelData = customModelData;
+        return this;
+    }
+
+    public ItemBuilder addItemFlags(ItemFlag... flags) {
+        this.itemFlags = flags;
+        return this;
+    }
+
+    /*
+     * Misc
+     * Unbreakable, Durability, Enchantments
+     */
+
+    public ItemBuilder setUnbreakable(boolean unbreakable) {
+        this.unbreakable = unbreakable;
+        return this;
+    }
+
+    public ItemBuilder setDamage(int damage) {
+        this.damage = damage;
+        return this;
+    }
+
+    public ItemBuilder enchant(Enchantment enchantment, int level) {
+        this.enchants.put(enchantment, level);
+        return this;
+    }
+
+
+
+
+    /*
+     * Builder
+     */
+
+    public ItemStack build() {
+        ItemStack item = new ItemStack(this.material);
+        ItemMeta itemMeta = item.getItemMeta();
+
+        // Main things
+        if (this.name != null) itemMeta.displayName(this.name);
+        item.setAmount(this.amount);
+        if (!this.lore.isEmpty()) itemMeta.lore(this.lore);
+
+        // Appearance
+        if (customModelData != null) itemMeta.setCustomModelData(customModelData);
+        if (this.itemFlags != null) itemMeta.addItemFlags(this.itemFlags);
+
+
+        // Misc
+        itemMeta.setUnbreakable(this.unbreakable);
+        if (itemMeta instanceof Damageable) {
+           ((Damageable) itemMeta).setDamage(this.damage);
+        }
+        item.addEnchantments(this.enchants);
+
+
+        item.setItemMeta(itemMeta);
+        return item;
+    }
+}
