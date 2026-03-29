9b05971f31afef27968b8fb5fe195420b1b76d75
Appa <1pewdiepieisbetter1@gmail.com>
2026-03-29 18:11:01 -0400

    Initial -- Shade

--- src/main/java/org/appa/appasUsefulThings/AppasUsefulThings.java
+++ src/main/java/org/appa/appasUsefulThings/AppasUsefulThings.java
@@ -1,66 +1,21 @@
 package org.appa.appasUsefulThings;
 
-import org.appa.appasUsefulThings.commands.ConfigCommand;
-import org.appa.appasUsefulThings.config.Config;
-import org.appa.appasUsefulThings.cooldownManager.example.ExampleCooldownCommand;
 import org.appa.appasUsefulThings.guiManager.GuiManager;
-import org.appa.appasUsefulThings.guiManager.example.ExampleGuiCommand;
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.io.File;
-
-import static org.appa.appasUsefulThings.config.Config.checkConfig;
 
 
 public final class  AppasUsefulThings extends JavaPlugin {
-    public static AppasUsefulThings plugin;
-    public static JavaPlugin getPlugin() {
-        return plugin;
-    }
+    private static Logger logger;
+    private static GuiManager guiManager;
 
-    public static Logger logger;
-    public static Logger getMyLogger() {
-        return logger;
-    } // Horrid name
-
-    public static File configFile;
-    public static File getConfigFile() {
-        return configFile;
-    }
-
-    public static YamlConfiguration config;
-    public YamlConfiguration getConfig() {
-        return config;
-    }
-
-    public static GuiManager guiManager;
-    public static GuiManager getGuiManager() {
-        return guiManager;
-    }
-
-
-    @Override
-    public void onEnable() {
-        plugin = this;
-        logger = new Logger(this);
+    public static void initalize(JavaPlugin plugin) {
+        logger = new Logger(plugin);
         guiManager = new GuiManager();
-        logger.log("Appa's useful things has loaded!");
-
+        logger.log("Appa's Useful Things has Loaded!");
 
         Bukkit.getPluginManager().registerEvents(
-                guiManager, this
+                guiManager, plugin
         );
-
-        new ConfigCommand().register(this);
-        new ExampleGuiCommand().register(this);
-        new ExampleCooldownCommand().register(this);
-
-
-        configFile = new File(getDataFolder(), "config.yml");
-        config = Config.create(this, configFile, false);
-
-        checkConfig();
     }
 }
