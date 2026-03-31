7b832acfecf91031f753d4ba035d67e51657f365
Appa <1pewdiepieisbetter1@gmail.com>
2026-03-30 22:03:32 -0400

    Added a 'builder' for config. Will add docs soon

--- src/main/java/org/appa/appasUsefulThings/AppasUsefulThings.java
+++ src/main/java/org/appa/appasUsefulThings/AppasUsefulThings.java
@@ -1,21 +1,56 @@
 package org.appa.appasUsefulThings;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
 import org.appa.appasUsefulThings.guiManager.GuiManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+public final class AppasUsefulThings {
+    private boolean enableBuildLogging = false;
+    private boolean enableGuiManager = false;
 
-public final class  AppasUsefulThings extends JavaPlugin {
-    private static Logger logger;
-    private static GuiManager guiManager;
+    private Component prefix = Component.text("[", NamedTextColor.WHITE)
+            .append(Component.text("AUT", NamedTextColor.GOLD))
+            .append(Component.text("]", NamedTextColor.WHITE));
 
-    public static void initalize(JavaPlugin plugin) {
-        logger = new Logger(plugin);
-        guiManager = new GuiManager();
-        logger.log("Appa's Useful Things has Loaded!");
+    private TextColor enabledColor = TextColor.color(0,182,217);
 
-        Bukkit.getPluginManager().registerEvents(
-                guiManager, plugin
-        );
+    public static AppasUsefulThings builder() {
+        return new AppasUsefulThings();
+    }
+
+    public AppasUsefulThings enableBuildLogging() {
+        this.enableBuildLogging = true;
+        return this;
+    }
+
+    public AppasUsefulThings enableGuiManager() {
+        this.enableGuiManager = true;
+        return this;
+    }
+
+    public void build(JavaPlugin plugin) {
+        if (enableGuiManager) {
+            Bukkit.getPluginManager().registerEvents(new GuiManager(), plugin);
+        }
+
+
+        if (!(enableBuildLogging)) { return; }
+
+        Logger logger = Logger.builder(plugin)
+                .setPrefix(prefix)
+                .build();
+
+        logger.log(Component.text("Appa's Useful Things is enabled."));
+
+        if (enableGuiManager) {
+            logger.log(Component.text("Gui Manager ").color(enabledColor)
+                    .append(Component.text(" successfully enabled")).color(NamedTextColor.GREEN)
+            );
+        }
+
     }
 }
+
