f0de513f0c47693fd609fca32f0160f57322a45b
Appa <1pewdiepieisbetter1@gmail.com>
2026-04-03 20:06:07 -0400

    formats. going afk

--- src/main/java/org/appa/appasUsefulThings/cooldownManager/TimeFormatter.java
+++ src/main/java/org/appa/appasUsefulThings/cooldownManager/TimeFormatter.java
@@ -1,4 +1,19 @@
 package org.appa.appasUsefulThings.cooldownManager;
 
 public class TimeFormatter {
+
+    public enum Format {
+        LONG,   // "1 hour, 2 minutes, 3 seconds"
+        SHORT,  // "1h 2m 3s"
+        CUSTOM  // custom labels
+    }
+
+    private static final String[][] LABELS = {
+            // Format.LONG
+            {"hour", "hours", "minute", "minutes", "second", "seconds"},
+            // Format.SHORT
+            {"h", "h", "m", "m", "s", "s"}
+    };
+
+    
 }
