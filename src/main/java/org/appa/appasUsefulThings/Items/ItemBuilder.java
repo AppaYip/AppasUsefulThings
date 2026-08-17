c1e9e6cdbf318b83152221bc12d8cbad88f34753
Appa <1pewdiepieisbetter1@gmail.com>
2026-08-17 03:00:38 -0400

    Added Javadoc and annotations

--- src/main/java/org/appa/appasUsefulThings/Items/ItemBuilder.java
+++ src/main/java/org/appa/appasUsefulThings/Items/ItemBuilder.java
@@ -7,35 +7,45 @@ import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * A builder to make creating items simple. Methods should be self-explanatory
+ */
+@NullMarked
 @SuppressWarnings("unused")
 public class ItemBuilder {
     private final Material material;
 
     // Main
-    private Component name;
+    @Nullable private Component name = null;
     private int amount = 1;
-    private List<Component> lore = new ArrayList<>();
+    private final List<Component> lore = new ArrayList<>();
 
     // Appearance
-    private Integer customModelData = null;
-    private ItemFlag[] itemFlags;
+    private ItemFlag[] itemFlags = new ItemFlag[0];
 
     // Misc
     private boolean unbreakable = false;
     private int damage = 0;
-    private HashMap<Enchantment, Integer> enchants = new HashMap<>();
+    private final HashMap<Enchantment, Integer> enchants = new HashMap<>();
     private boolean hideToolTip = false;
 
 
 
-    /*
-     * Constructors
+    /* Constructors */
+
+    /**
+     * ItemBuilder entry point.
+     * @param material The material to use for the item.
+     * @param name The name of the item.
+     * @param damage The damage to the item.
      */
     public ItemBuilder(Material material, Component name, int damage) {
         this.material = material;
@@ -43,87 +53,131 @@ public class ItemBuilder {
         this.damage = damage;
     }
 
+    /**
+     * ItemBuilder entry point.
+     * @param material The material to use for the item.
+     * @param name The name of the item.
+     */
     public ItemBuilder(Material material, Component name) {
         this.material = material;
         this.name = name;
     }
 
+    /**
+     * ItemBuilder entry point.
+     * @param material The material to use for the item.
+     * @param damage The damage to the item.
+     */
     public ItemBuilder(Material material, int damage) {
         this.material = material;
         this.damage = damage;
     }
 
-
+    /**
+     * ItemBuilder entry point.
+     * @param material The material to use for the item.
+     */
     public ItemBuilder(Material material) {
         this.material = material;
     }
 
 
-    /*
-     * Main Things
-     * Name, Amount, Lore
+    /* Main Things: Name, Amount, Lore */
+
+    /**
+     * Sets the display name for the current item.
+     * @param name The name of the item.
+     * @return Instance of the ItemBuilder.
      */
     public ItemBuilder setDisplayName(Component name) {
         this.name = name;
         return this;
     }
 
+    /**
+     * Sets the stack size of the item.
+     * @param amount The amount to set to.
+     * @return Instance of the ItemBuilder.
+     */
     public ItemBuilder setAmount(int amount) {
         this.amount = amount;
         return this;
     }
 
-
+    /**
+     * Adds lore to the item.
+     * @param line The line[s] to add.
+     * @return Instance of the ItemBuilder.
+     */
     public ItemBuilder addLore(Component... line) {
         this.lore.addAll(Arrays.asList(line));
         return this;
     }
 
 
-    /*
-     * Appearance
-     * Custom Model Data, Item Flags
-     */
-    public ItemBuilder setCustomModelData(int customModelData) {
-        this.customModelData = customModelData;
-        return this;
-    }
+    /* Appearance: Custom Model Data, Item Flags */
 
+
+    /**
+     * Adds item flags to the item.
+     * @param flags The flag[s] to add.
+     * @return Instance of the ItemBuilder.
+     */
     public ItemBuilder addItemFlags(ItemFlag... flags) {
         this.itemFlags = flags;
         return this;
     }
 
-    /*
-     * Misc
-     * Unbreakable, Durability, Enchantments
-     */
+    /* Misc: Unbreakable, Durability, Enchantments */
 
+    /**
+     * Makes the item unbreakable.
+     * @param unbreakable Whether to make the item unbreakable.
+     * @return Instance of the ItemBuilder.
+     */
     public ItemBuilder setUnbreakable(boolean unbreakable) {
         this.unbreakable = unbreakable;
         return this;
     }
 
+    /**
+     * Sets the item's damage.
+     * @param damage The damage.
+     * @return Instance of the ItemBuilder.
+     */
     public ItemBuilder setDamage(int damage) {
         this.damage = damage;
         return this;
     }
 
+    /**
+     * Adds an enchantment to the item.
+     * @param enchantment The enchantment to add.
+     * @param level The level of enchantment.
+     * @return Instance of the ItemBuilder.
+     */
     public ItemBuilder enchant(Enchantment enchantment, int level) {
         this.enchants.put(enchantment, level);
         return this;
     }
 
+    /**
+     * Hides the tooltip of the item.
+     * @param hideToolTip Whether to hide the tooltip of the item.
+     * @return Instance of the ItemBuilder.
+     */
     public ItemBuilder hideToolTip(boolean hideToolTip) {
         this.hideToolTip = hideToolTip;
         return this;
     }
 
 
-    /*
-     * Builder
-     */
+    /* Builder */
 
+    /**
+     * Builds the final item.
+     * @return The final {@link ItemStack}
+     */
     public ItemStack build() {
         ItemStack item = new ItemStack(this.material);
         ItemMeta itemMeta = item.getItemMeta();
@@ -134,8 +188,7 @@ public class ItemBuilder {
         if (!this.lore.isEmpty()) itemMeta.lore(this.lore);
 
         // Appearance
-        if (customModelData != null) itemMeta.setCustomModelData(customModelData);
-        if (this.itemFlags != null) itemMeta.addItemFlags(this.itemFlags);
+        if (this.itemFlags.length == 0) itemMeta.addItemFlags(this.itemFlags);
 
 
         // Misc
