f67c7310d8b296d543bb6578d98367598a19f32d
Appa <1pewdiepieisbetter1@gmail.com>
2026-08-21 02:09:33 -0400

    Added javadocs for main class, also switched to lombok getter

--- src/main/java/org/appa/appasUsefulThings/AppasUsefulThings.java
+++ src/main/java/org/appa/appasUsefulThings/AppasUsefulThings.java
@@ -1,15 +1,15 @@
 package org.appa.appasUsefulThings;
 
+import lombok.Getter;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextColor;
 import org.appa.appasUsefulThings.guiManager.GuiManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @SuppressWarnings("unused")
 public final class AppasUsefulThings {
-    private GuiManager guiManager;
+    @Getter private GuiManager guiManager;
 
     private boolean enableBuildLogging = false;
     private boolean enableGuiManager = false;
@@ -18,27 +18,38 @@ public final class AppasUsefulThings {
             .append(Component.text("AUT", NamedTextColor.GOLD))
             .append(Component.text("]", NamedTextColor.WHITE));
 
-    private final TextColor enabledColor = TextColor.color(0,182,217);
-
+    /**
+     * Gets a new instance. This is the main entry point.
+     * @return Instance of {@link AppasUsefulThings}
+     */
     public static AppasUsefulThings builder() {
         return new AppasUsefulThings();
     }
 
+    /**
+     * Enables build logging. For now this doesn't really do too much.
+     * @return Instance of {@link AppasUsefulThings}
+     */
     public AppasUsefulThings enableBuildLogging() {
         this.enableBuildLogging = true;
         return this;
     }
 
+    /**
+     * Allows you to use {@link GuiManager}.
+     * @return Instance of {@link AppasUsefulThings}
+     */
     public AppasUsefulThings enableGuiManager() {
         this.enableGuiManager = true;
         return this;
     }
 
-    public GuiManager getGuiManager() {
-        return guiManager;
-    }
 
-
+    /**
+     * Finishes building your instance.
+     * @param plugin An instance of your plugin.
+     * @return Instance of {@link AppasUsefulThings}
+     */
     public AppasUsefulThings build(JavaPlugin plugin) {
         if (enableGuiManager) {
             guiManager = new GuiManager();
@@ -56,7 +67,7 @@ public final class AppasUsefulThings {
         logger.log(Component.text("Appa's Useful Things is enabled."));
 
         if (enableGuiManager) {
-            logger.log(Component.text("Gui Manager successfully enabled").color(enabledColor)
+                logger.log(Component.text("Gui Manager successfully enabled")
             );
         }
 
