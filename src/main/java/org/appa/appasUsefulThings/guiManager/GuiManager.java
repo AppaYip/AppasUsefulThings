bab0d46b03620806297a8f72e3a91daa5c9e5407
AppaYip <1pewdiepieisbetter1@gmail.com>
2026-08-25 02:35:44 -0400

    Thank you intellij for not importing the classes

--- src/main/java/org/appa/appasUsefulThings/guiManager/GuiManager.java
+++ src/main/java/org/appa/appasUsefulThings/guiManager/GuiManager.java
@@ -1,7 +1,12 @@
 package org.appa.appasUsefulThings.guiManager;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.Inventory;
 import org.jspecify.annotations.NullMarked;
 
