81e4b61784843dc4b69f5f35df556fdd7f208289
AppaYip <1pewdiepieisbetter1@gmail.com>
2026-08-25 03:24:03 -0400

    Lwk calling bs on what borp said

--- src/main/java/org/appa/appasUsefulThings/AppasUsefulThings.java
+++ src/main/java/org/appa/appasUsefulThings/AppasUsefulThings.java
@@ -1,11 +1,12 @@
 package org.appa.appasUsefulThings;
 
+import lombok.NonNull;
 import org.appa.appasUsefulThings.guiManager.GuiManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class AppasUsefulThings extends JavaPlugin {
-    public AppasUsefulThings() {
-        Bukkit.getPluginManager().registerEvents(new GuiManager(), this);
+public final class AppasUsefulThings {
+    public AppasUsefulThings(@NonNull JavaPlugin plugin) {
+        Bukkit.getPluginManager().registerEvents(new GuiManager(), plugin);
     }
 }
