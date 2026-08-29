5ed248a5adae00e07a4ca45d379016aa24a0d338
Appa <1pewdiepieisbetter1@gmail.com>
2026-08-28 21:38:14 -0400

    We rewrote the entire thing

--- src/main/java/org/appa/appasUsefulThings/Items/ItemBuilder.java
+++ src/main/java/org/appa/appasUsefulThings/Items/ItemBuilder.java
@@ -9,255 +9,263 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.components.CustomModelDataComponent;
-import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.NonNull;
 
 import java.util.*;
+import java.util.function.Consumer;
 
 /**
  * A builder to make creating items simple. Methods have Javadocs, however they should be self-explanatory.
  */
-@NullMarked
-@SuppressWarnings({"unused", "UnstableApiUsage"})
+@SuppressWarnings({"UnstableApiUsage", "unused"})
 public class ItemBuilder {
-    private final Material material;
-
-    // Main
-    private Component name = Component.empty();
-    private int amount = 1;
-    private final List<Component> lore = new ArrayList<>();
-
-    // Appearance
-    private ItemFlag[] itemFlags = new ItemFlag[0];
-
-    private final List<Float> customModelDataFloats = new ArrayList<>();
-    private final List<Boolean> customModelDataFlags = new ArrayList<>();
-    private final List<String> customModelDataStrings = new ArrayList<>();
-    private final List<Color> customModelDataColors = new ArrayList<>();
-
-    // Misc
-    private boolean unbreakable = false;
-    private int damage = 0;
-    private final Map<Enchantment, Integer> enchants = new HashMap<>();
-    private boolean hideToolTip = false;
-
-
-
-    /* Constructors */
-
-    /**
-     * ItemBuilder entry point.
-     * @param material The material to use for the item.
-     * @param name The name of the item.
-     * @param damage The damage to the item.
-     */
-    public ItemBuilder(Material material, Component name, int damage) {
-        this.material = material;
-        this.name = name;
-        this.damage = damage;
+    private ItemBuilder() {
+        // Utility Class
     }
 
     /**
-     * ItemBuilder entry point.
-     * @param material The material to use for the item.
-     * @param name The name of the item.
+     * Creates an item using the applied builder.
+     *
+     * @param material The material of the item.
+     * @param consumer The item configuration.
+     * @return the built item.
      */
-    public ItemBuilder(Material material, Component name) {
-        this.material = material;
-        this.name = name;
-    }
-
-    /**
-     * ItemBuilder entry point.
-     * @param material The material to use for the item.
-     * @param damage The damage to the item.
-     */
-    public ItemBuilder(Material material, int damage) {
-        this.material = material;
-        this.damage = damage;
-    }
-
-    /**
-     * ItemBuilder entry point.
-     * @param material The material to use for the item.
-     */
-    public ItemBuilder(Material material) {
-        this.material = material;
+    public static ItemStack of(@NonNull Material material, @NonNull Consumer<Item> consumer) {
+        return new Item(material)
+                .apply(consumer)
+                .build();
     }
 
 
-    /* Main Things: Name, Amount, Lore */
+    @SuppressWarnings("UnusedReturnValue")
+    public static final class Item {
+        private final Material material;
 
-    /**
-     * Sets the display name for the current item.
-     * @param name The name of the item.
-     * @return Instance of the ItemBuilder.
-     */
-    public ItemBuilder setDisplayName(Component name) {
-        this.name = name;
-        return this;
-    }
-
-    /**
-     * Sets the stack size of the item.
-     * @param amount The amount to set to.
-     * @return Instance of the ItemBuilder.
-     */
-    public ItemBuilder setAmount(int amount) {
-        this.amount = amount;
-        return this;
-    }
-
-    /**
-     * Adds lore to the item.
-     * @param line The line[s] to add.
-     * @return Instance of the ItemBuilder.
-     */
-    public ItemBuilder addLore(Component... line) {
-        this.lore.addAll(Arrays.asList(line));
-        return this;
-    }
-
-
-    /* Appearance: Custom Model Data, Item Flags */
-
-    /**
-     * Appends floats to the item's custom model data.
-     * @param values floats to add to the item's custom model data.
-     * @return Instance of the ItemBuilder.
-     */
-    public ItemBuilder customModelData(float... values) {
-        for (float value : values) {
-            this.customModelDataFloats.add(value);
-        }
-        return this;
-    }
-
-    /**
-     * Appends booleans to the item's custom model data.
-     * @param values booleans to add to the item's custom model data.
-     * @return Instance of the ItemBuilder.
-     */
-    public ItemBuilder customModelData(boolean... values) {
-        for (boolean value : values) {
-            this.customModelDataFlags.add(value);
-        }
-        return this;
-    }
-
-    /**
-     * Appends strings to the item's custom model data.
-     * @param values strings to add to the item's custom model data.
-     * @return Instance of the ItemBuilder.
-     */
-    public ItemBuilder customModelData(String... values) {
-        this.customModelDataStrings.addAll(Arrays.asList(values));
-        return this;
-    }
-
-    /**
-     * Appends floats to the item's custom model data.
-     * @param values floats to add to the item's custom model data.
-     * @return Instance of the ItemBuilder.
-     */
-    public ItemBuilder customModelData(Color... values) {
-        this.customModelDataColors.addAll(Arrays.asList(values));
-        return this;
-    }
-
-
-    /**
-     * Adds item flags to the item.
-     * @param flags The flag[s] to add.
-     * @return Instance of the ItemBuilder.
-     */
-    public ItemBuilder setItemFlags(ItemFlag... flags) {
-        this.itemFlags = flags;
-        return this;
-    }
-
-    /* Misc: Unbreakable, Durability, Enchantments */
-
-    /**
-     * Makes the item unbreakable.
-     * @param unbreakable Whether to make the item unbreakable.
-     * @return Instance of the ItemBuilder.
-     */
-    public ItemBuilder setUnbreakable(boolean unbreakable) {
-        this.unbreakable = unbreakable;
-        return this;
-    }
-
-    /**
-     * Sets the item's damage.
-     * @param damage The damage.
-     * @return Instance of the ItemBuilder.
-     */
-    public ItemBuilder setDamage(int damage) {
-        this.damage = damage;
-        return this;
-    }
-
-    /**
-     * Adds an enchantment to the item.
-     * @param enchantment The enchantment to add.
-     * @param level The level of enchantment.
-     * @return Instance of the ItemBuilder.
-     */
-    public ItemBuilder enchant(Enchantment enchantment, int level) {
-        this.enchants.put(enchantment, level);
-        return this;
-    }
-
-    /**
-     * Hides the tooltip of the item.
-     * @param hideToolTip Whether to hide the tooltip of the item.
-     * @return Instance of the ItemBuilder.
-     */
-    public ItemBuilder hideToolTip(boolean hideToolTip) {
-        this.hideToolTip = hideToolTip;
-        return this;
-    }
-
-
-    /* Builder */
-
-    /**
-     * Builds the final item.
-     * @return The final {@link ItemStack}
-     */
-    public ItemStack build() {
-        ItemStack item = new ItemStack(this.material);
-        ItemMeta itemMeta = item.getItemMeta();
-
-        // Main things
-        if (!this.name.equals(Component.empty())) itemMeta.displayName(this.name);
-
-        item.setAmount(this.amount);
-        if (!this.lore.isEmpty()) itemMeta.lore(this.lore);
-
-        // Appearance
-        if (this.itemFlags.length > 0) itemMeta.addItemFlags(this.itemFlags);
-
-        CustomModelDataComponent customModelDataComponent = itemMeta.getCustomModelDataComponent();
-
-        customModelDataComponent.setFloats(this.customModelDataFloats);
-        customModelDataComponent.setFlags(this.customModelDataFlags);
-        customModelDataComponent.setStrings(this.customModelDataStrings);
-        customModelDataComponent.setColors(this.customModelDataColors);
-
-        itemMeta.setCustomModelDataComponent(customModelDataComponent);
-
+        private Component name;
+        private int amount = 1;
+        private final List<Component> lore = new ArrayList<>();
 
         // Misc
-        itemMeta.setUnbreakable(this.unbreakable);
-        if (itemMeta instanceof Damageable damageable) {
-            damageable.setDamage(this.damage);
+        private boolean unbreakable;
+        private int damage;
+        private boolean hideToolTip;
+
+        private final Appearance appearance = new Appearance();
+        private final Enchantments enchantments = new Enchantments();
+
+
+        private Item(@NonNull Material material) {
+            this.material = material;
         }
-        item.addEnchantments(this.enchants);
 
-        itemMeta.setHideTooltip(this.hideToolTip);
+        private Item apply(@NonNull Consumer<Item> consumer) {
+            consumer.accept(this);
+            return this;
+        }
 
-        item.setItemMeta(itemMeta);
-        return item;
+
+        /**
+         * Sets the name of the item. Internally this will make a component.
+         * @param name The {@link String} to set the item to.
+         */
+        public Item name(@NonNull String name) {
+            this.name = Component.text(name);
+            return this;
+        }
+
+        /**
+         * Sets the name of the item.
+         * @param name The {@link Component} to set the name to.
+         */
+        public Item name(@NonNull Component name) {
+            this.name = name;
+            return this;
+        }
+
+        /**
+         * Sets the count of the item.
+         * @param amount The amount of the item.
+         */
+        public Item amount(int amount) {
+            this.amount = amount;
+            return this;
+        }
+
+        /**
+         * Adds lore to the item.
+         * @param lore The {@link String}s to add to the item's lore.
+         */
+        public Item lore(@NonNull String... lore) {
+            for (String l : lore) {
+                this.lore.add(Component.text(l));
+            }
+            return this;
+        }
+
+        /**
+         * Adds lore to the item.
+         * @param lines The {@link Component}s to add to the item's lore.
+         */
+        public Item lore(@NonNull Component... lines) {
+            this.lore.addAll(Arrays.asList(lines));
+            return this;
+        }
+
+        /**
+         * Creates an appearance object.
+         * @param appearance The configuration for the item's appearance.
+         */
+        public Item appearance(@NonNull Consumer<Appearance> appearance) {
+            appearance.accept(this.appearance);
+            return this;
+        }
+
+        /**
+         *  Creates an enchantment object.
+         * @param consumer Enchantment configuration.
+         */
+        public Item enchantments(@NonNull Consumer<Enchantments> consumer) {
+            consumer.accept(this.enchantments);
+            return this;
+        }
+
+        /**
+         * Makes the item unbreakable.
+         */
+        public Item unbreakable() {
+            this.unbreakable = true;
+            return this;
+        }
+
+        /**
+         * Sets the damage to the item.
+         * @param damage The amount of damage.
+         */
+        public Item damage(int damage) {
+            this.damage = damage;
+            return this;
+        }
+
+        /**
+         * Hides the tooltip of the item.
+         */
+        public Item hideToolTip() {
+            this.hideToolTip = true;
+            return this;
+        }
+
+        public ItemStack build() {
+            ItemStack item = new ItemStack(this.material);
+            ItemMeta meta = item.getItemMeta();
+
+            applyBasicProperties(meta);
+            applyBasicAppearance(meta);
+            applyMiscProperties(meta);
+
+            item.setAmount(this.amount);
+            item.setItemMeta(meta);
+
+            return item;
+        }
+
+        private void applyBasicProperties(@NonNull ItemMeta meta) {
+            if (this.name != null) {
+                meta.displayName(this.name);
+            }
+
+            if (!this.lore.isEmpty()) {
+                meta.lore(this.lore);
+            }
+        }
+
+        private void applyBasicAppearance(@NonNull ItemMeta meta) {
+            if (this.appearance.itemFlags.length > 0) {
+                meta.addItemFlags(this.appearance.itemFlags);
+            }
+
+            CustomModelDataComponent customModelData =
+                    meta.getCustomModelDataComponent();
+
+            customModelData.setFloats(this.appearance.modelData.customModelDataFloats);
+            customModelData.setFlags(this.appearance.modelData.customModelDataFlags);
+            customModelData.setStrings(this.appearance.modelData.customModelDataStrings);
+            customModelData.setColors(this.appearance.modelData.customModelDataColors);
+
+            meta.setCustomModelDataComponent(customModelData);
+        }
+
+        private void applyMiscProperties(@NonNull ItemMeta meta) {
+            meta.setUnbreakable(this.unbreakable);
+            meta.setHideTooltip(this.hideToolTip);
+
+            if (meta instanceof Damageable damageable) {
+                damageable.setDamage(this.damage);
+            }
+
+            if (!this.enchantments.enchants.isEmpty()) {
+                for (Map.Entry<Enchantment, Integer> entry : enchantments.enchants.entrySet()) {
+                    meta.addEnchant(
+                            entry.getKey(),
+                            entry.getValue(),
+                            true
+                    );
+                }
+            }
+        }
+
+
+        // Appearance Object
+        public static final class Appearance {
+            private ItemFlag[] itemFlags = new ItemFlag[0];
+            private final ModelData modelData = new ModelData();
+
+            public Appearance flags(@NonNull ItemFlag... itemFlags) {
+                this.itemFlags = itemFlags;
+                return this;
+            }
+
+            public Appearance modelData(@NonNull Consumer<ModelData> consumer) {
+                consumer.accept(this.modelData);
+                return this;
+            }
+        }
+
+        public static final class ModelData {
+            private final List<Float> customModelDataFloats = new ArrayList<>();
+            private final List<Boolean> customModelDataFlags = new ArrayList<>();
+            private final List<String> customModelDataStrings = new ArrayList<>();
+            private final List<Color> customModelDataColors = new ArrayList<>();
+
+            public ModelData floats(Float... floats) {
+                this.customModelDataFloats.addAll(Arrays.asList(floats));
+                return this;
+            }
+
+            public ModelData flags(Boolean... flags) {
+                this.customModelDataFlags.addAll(Arrays.asList(flags));
+                return this;
+            }
+
+            public ModelData strings(String... strings) {
+                this.customModelDataStrings.addAll(Arrays.asList(strings));
+                return this;
+            }
+
+            public ModelData color(Color... color) {
+                this.customModelDataColors.addAll(Arrays.asList(color));
+                return this;
+            }
+        }
+
+
+        // Enchants Object
+        public static final class Enchantments {
+            private final Map<Enchantment, Integer> enchants = new HashMap<>();
+
+            public Enchantments add(@NonNull Enchantment enchant, int level) {
+                this.enchants.put(enchant, level);
+                return this;
+            }
+        }
     }
 }
