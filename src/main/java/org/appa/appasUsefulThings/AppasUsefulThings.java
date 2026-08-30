2b4605c5fd4460a6eaffea5f9cee931701f2bc80
Appa <1pewdiepieisbetter1@gmail.com>
2026-08-29 21:01:17 -0400

    Rewriting BPT
    Nuked Command.

--- src/main/java/org/appa/appasUsefulThings/AppasUsefulThings.java
+++ src/main/java/org/appa/appasUsefulThings/AppasUsefulThings.java
@@ -1,12 +1,16 @@
 package org.appa.appasUsefulThings;
 
+import lombok.Getter;
 import lombok.NonNull;
 import org.appa.appasUsefulThings.guiManager.GuiManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AppasUsefulThings {
+    @Getter private static JavaPlugin plugin;
+
     public AppasUsefulThings(@NonNull JavaPlugin plugin) {
+        AppasUsefulThings.plugin = plugin;
         Bukkit.getPluginManager().registerEvents(new GuiManager(), plugin);
     }
 }
