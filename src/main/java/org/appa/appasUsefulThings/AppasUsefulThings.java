70ff4110b1576415153b9aedafb67f03a84d0f86
Appa <1pewdiepieisbetter1@gmail.com>
2026-08-29 21:06:53 -0400

    Added getter for GuiManager

--- src/main/java/org/appa/appasUsefulThings/AppasUsefulThings.java
+++ src/main/java/org/appa/appasUsefulThings/AppasUsefulThings.java
@@ -8,9 +8,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AppasUsefulThings {
     @Getter private static JavaPlugin plugin;
+    private static GuiManager guiManager;
 
     public AppasUsefulThings(@NonNull JavaPlugin plugin) {
         AppasUsefulThings.plugin = plugin;
-        Bukkit.getPluginManager().registerEvents(new GuiManager(), plugin);
+    }
+
+    /**
+     * Gets an instance of {@link GuiManager}. This lets you use Gui Elements.
+     *
+     * @return An instance of {@link GuiManager}. This **will** use the same instance every time this is called.
+     */
+    public static GuiManager getGuiManager() {
+        if (guiManager == null) {
+            guiManager = new GuiManager();
+            Bukkit.getPluginManager().registerEvents(guiManager, plugin);
+        }
+
+        return guiManager;
     }
 }
